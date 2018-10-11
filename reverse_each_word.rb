a3fa1b8e512e5a9621ba61a7e808bf6fc8331d05
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 05:15:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(arg)
   str_answer = String.new("")
   tmp = arg.split(" ")
   tmp.each do |word|
-    str_answer << "#{word}".reverse
+    str_answer << "#{word}".reverse + " "
   end
   str_answer
 end
