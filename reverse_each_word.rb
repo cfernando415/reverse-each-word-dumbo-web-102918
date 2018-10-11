610f06fc40e42cbd77e076aa65e10ee2a44d5976
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 05:15:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(arg)
-  tmp = arg.split(%r{\s*})
-  counter = tmp.size
   str_answer = String.new("")
-  while counter > 0
-    str_answer << tmp[counter - 1]
-    counter -= 1
+  tmp = arg.split(" ")
+  tmp.each do |word|
+    str_answer << "#{word}".reverse
   end
   str_answer
 end
