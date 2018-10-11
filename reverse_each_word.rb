9dcb896391dc991e865e022410605c59eac0431a
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 05:10:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(arg)
   tmp = arg.split(%r{\s*})
-  
+  counter = tmp.size
+  str_answer = String.new("")
+  while counter > 0
+    str_answer << tmp[counter - 1]
+    counter -= 1
+  end
+  puts str_answer
 end
